8a34f2ef1236536b285791172aa6d52a7b5572d4
Petrov-Daniil <zara123486@mail.ru>
2021-04-11 21:18:22 +0700

    Add file read

--- src/include/asm.h
+++ src/include/asm.h
@@ -3,4 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 #include "cu.h"
+
+int cmd_search(const char *command);
+int asm_to_object(const char *filename_asm, const char *filename_object);
+
 #endif
--- src/include/cu.h
+++ src/include/cu.h
@@ -1,19 +1,21 @@
 #ifndef CU
 #define CU
 #include "printConsole.h"
-#define READ 0x10 
-#define WRITE 0X11
-#define LOAD 0X20
-#define STORE 0X21
-#define ADD 0X30
-#define SUB 0X31
-#define DIVIDE 0X32
-#define MUL 0X33
-#define JUMP 0X40
-#define JNEG 0X41
+
+#define READ 0x10
+#define WRITE 0x11
+#define LOAD 0x20
+#define STORE 0x21
+#define ADD 0x30
+#define SUB 0x31
+#define DIVIDE 0x32
+#define MUL 0x33
+#define JUMP 0x40
+#define JNEG 0x41
 #define JZ 0x42
 #define HALT 0x43
 #define JNP 0x59
+
 int Cu();
 int Alu(int command, int operand);
 
